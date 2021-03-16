4ca5a6567cb0e90e2730d6542513023e74e8a2cb
Sean <74019460+hawaiiankid@users.noreply.github.com>
2021-03-16 11:13:46 -0700

    Add files via upload

---
+++ L02HandsOn_zip/L02HandsOn/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+ReactDOM.render(
+    stateInfo('Idaho', 'Tennessee', 'Maine', 'Wisconsin'),
+    document.getElementById('root')
+);
+
+
